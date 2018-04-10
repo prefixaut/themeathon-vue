1cad7f2b438e940a50edb124b2338a0297ee734e
PreFiXAUT <me@prefixaut.net>
2018-04-10 22:30:27 +0200

    Added Channel/Info settings to environment

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,8 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  TWITCH_CHANNEL: '"themeathon"',
+  DISCORD_LINK: '"https://discordapp.com/invite/wd5GwXK"',
+  TWITTER_NAME: '"themeathon"',
+  YOUTUBE_LINK: '"https://www.youtube.com/channel/UCoeWJ8x6jM_tN5AnBqLzQfg"',
 }
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,29 +9,34 @@
         </div>
 
         <div class="social-media">
-            <a class="social discord" href="https://discordapp.com/invite/wd5GwXK" target="_blank">
+            <a v-if="discord" class="social discord" :href="discord" title="Discord Invite-Link" target="_blank">
                 <i class="icon fab fa-discord"></i>
             </a>
-            <a class="social twitch" href="https://www.twitch.tv/Themeathon" target="_blank">
+            <a v-if="twitch" class="social twitch" :href="'https://www.twitch.tv/' + twitch" :title="twitch" target="_blank">
                 <i class="icon fab fa-twitch"></i>
             </a>
-            <a class="social twitter" href="https://twitter.com/Themeathon" target="_blank">
+            <a v-if="twitter" class="social twitter" :href="'https://twitter.com/' + twitter" :title="'@' + twitter" target="_blank">
                 <i class="icon fab fa-twitter"></i>
             </a>
-            <a class="social youtube" href="https://www.youtube.com/channel/UCoeWJ8x6jM_tN5AnBqLzQfg" target="_blank">
+            <a v-if="youtube" class="social youtube" :href="youtube" title="YouTube Channel" target="_blank">
                 <i class="icon fab fa-youtube"></i>
             </a>
         </div>
-
-        <nav class="menu-wrapper">
-            <ul class="menu">
-                <li>Who are we</li>
-                <li>Save the Children</li>
-                <li>Team</li>
-            </ul>
-        </nav>
     </header>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            discord: process.env.DISCORD_LINK,
+            twitch: process.env.TWITCH_CHANNEL,
+            twitter: process.env.TWITTER_NAME,
+            youtube: process.env.YOUTUBE_LINK,
+        };
+    }
+};
+</script>
 
 <style lang="scss">
 @import '../styles/config';
@@ -46,41 +51,48 @@ header {
     flex-wrap: nowrap;
 
     .social-media {
-        height: 100%;
-        flex: 0 0 auto;
+        display: none;
+    }
 
-        .social {
-            height: 100%;
-            width: 70px;
-            padding: 15px;
+    @include media-up('sm') {
+        .social-media {
             display: block;
-            box-sizing: border-box;
-            border-right: 1px solid $color-gray-dark;
-            text-align: center;
-            transition: 300ms;
-            float: left;
+            height: 100%;
+            flex: 0 0 auto;
 
-            &:last-child {
-                border-right: none;
-            }
-
-            .icon {
-                vertical-align: middle;
+            .social {
+                height: 100%;
+                width: 70px;
+                padding: 15px;
+                display: block;
+                box-sizing: border-box;
+                border-right: 1px solid $color-gray-dark;
+                text-align: center;
                 transition: 300ms;
-                font-size: 38px;
-            }
+                float: left;
 
-            @each $name, $color in $header-icons {
-                &.#{$name} {
-                    .icon {
-                        color: $color;
-                    }
+                &:last-child {
+                    border-right: none;
+                }
 
-                    &:hover {
-                        background: $color;
+                .icon {
+                    vertical-align: middle;
+                    transition: 300ms;
+                    font-size: 38px;
+                }
 
+                @each $name, $color in $header-icons {
+                    &.#{$name} {
                         .icon {
-                            color: $color-off-white;
+                            color: $color;
+                        }
+
+                        &:hover {
+                            background: $color;
+
+                            .icon {
+                                color: $color-off-white;
+                            }
                         }
                     }
                 }
@@ -104,10 +116,6 @@ header {
                 height: 100%;
             }
         }
-    }
-
-    .menu-wrapper {
-        display: none;
     }
 }
 </style>
